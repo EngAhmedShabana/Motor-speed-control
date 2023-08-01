fe97218104a48e07e5002bc8469593e2e508da1f
EngAhmedShabana <138723900+EngAhmedShabana@users.noreply.github.com>
2023-08-01 12:41:18 +0300

    Add files via upload

--- motor_speed_control1.ino
+++ motor_speed_control1.ino
@@ -2,15 +2,16 @@
 //
 const int trig =9;
 const int echo=8;
-const int speed1=5;
-const int speed2=6;
+const int speed1=3;
+const int speed2=5;
 long distance;
 long time;
 void setup()
 {pinMode(trig,OUTPUT);
  pinMode(echo,INPUT);
  pinMode(speed1,OUTPUT);
-  pinMode(speed2,OUTPUT);
+ pinMode(speed2,OUTPUT);
+ Serial.begin(9600);
 
   
 }
@@ -23,17 +24,21 @@ void loop(){
  digitalWrite(trig,LOW);
   time=pulseIn(echo,HIGH);
  distance=(time/2)*0.0343;
- if (distance<=200){analogWrite(speed1,0);
+ Serial.println(distance);
+  
+  if (distance>200){analogWrite(speed1,0);
             analogWrite(speed2,0);}
- else if (200<distance<150){analogWrite(speed1,64);
-                 analogWrite(speed2,0);}
-  else if (150<distance<100){analogWrite(speed1,127);
-            analogWrite(speed2,0);}
-  else if (100<distance<4){analogWrite(speed1,192);
-            analogWrite(speed2,0);}
-  else (distance<=4);
-  {analogWrite(speed1,255);
-              analogWrite(speed2,0);}
+ else if(200>=distance>=150){analogWrite(speed1,0);
+                 analogWrite(speed2,64);}
+  else if (150>distance>=100){analogWrite(speed1,0);
+            analogWrite(speed2,127);}
+  else if (100>distance>4)
+  {analogWrite(speed1,0);
+              analogWrite(speed2,194);}
+  else
+  {analogWrite(speed1,0);
+              analogWrite(speed2,255);}
+  
 
                  }
   
